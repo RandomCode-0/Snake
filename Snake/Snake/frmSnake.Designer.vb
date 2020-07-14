40ad3112b4fa7546690f5724f10a0e81d131bb1a
Random Code <68001635+RandomCode-0@users.noreply.github.com>
2020-07-14 00:17:57 -0400

    Add files via upload

---
+++ Snake/Snake/frmSnake.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSnake
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSnake))
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.tmrMain = New System.Windows.Forms.Timer(Me.components)
+        Me.lisSpace = New System.Windows.Forms.ListBox()
+        Me.lblHighScore = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.btnUp = New System.Windows.Forms.Button()
+        Me.btnLeft = New System.Windows.Forms.Button()
+        Me.btnDown = New System.Windows.Forms.Button()
+        Me.btnRight = New System.Windows.Forms.Button()
+        Me.lblDirection = New System.Windows.Forms.Label()
+        Me.lblSegments = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.tmrTime = New System.Windows.Forms.Timer(Me.components)
+        Me.grpInfo = New System.Windows.Forms.GroupBox()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.grpControls = New System.Windows.Forms.GroupBox()
+        Me.picPad = New System.Windows.Forms.PictureBox()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.grpInfo.SuspendLayout()
+        Me.grpControls.SuspendLayout()
+        CType(Me.picPad, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnStart.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStart.ForeColor = System.Drawing.Color.Fuchsia
+        Me.btnStart.Location = New System.Drawing.Point(584, 281)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(235, 121)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = False
+        '
+        'tmrMain
+        '
+        Me.tmrMain.Interval = 300
+        '
+        'lisSpace
+        '
+        Me.lisSpace.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lisSpace.FormattingEnabled = True
+        Me.lisSpace.ItemHeight = 18
+        Me.lisSpace.Location = New System.Drawing.Point(840, 12)
+        Me.lisSpace.Name = "lisSpace"
+        Me.lisSpace.Size = New System.Drawing.Size(560, 472)
+        Me.lisSpace.TabIndex = 1
+        Me.lisSpace.Visible = False
+        '
+        'lblHighScore
+        '
+        Me.lblHighScore.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblHighScore.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHighScore.Location = New System.Drawing.Point(6, 16)
+        Me.lblHighScore.Name = "lblHighScore"
+        Me.lblHighScore.Size = New System.Drawing.Size(249, 23)
+        Me.lblHighScore.TabIndex = 3
+        Me.lblHighScore.Text = "High Score:"
+        Me.lblHighScore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblTime
+        '
+        Me.lblTime.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblTime.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTime.Location = New System.Drawing.Point(6, 73)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(249, 23)
+        Me.lblTime.TabIndex = 4
+        Me.lblTime.Text = "Time: "
+        Me.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'btnUp
+        '
+        Me.btnUp.BackColor = System.Drawing.Color.Red
+        Me.btnUp.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUp.Location = New System.Drawing.Point(707, 375)
+        Me.btnUp.Name = "btnUp"
+        Me.btnUp.Size = New System.Drawing.Size(60, 60)
+        Me.btnUp.TabIndex = 5
+        Me.btnUp.Text = "UP"
+        Me.btnUp.UseVisualStyleBackColor = False
+        '
+        'btnLeft
+        '
+        Me.btnLeft.BackColor = System.Drawing.Color.Red
+        Me.btnLeft.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLeft.Location = New System.Drawing.Point(646, 436)
+        Me.btnLeft.Name = "btnLeft"
+        Me.btnLeft.Size = New System.Drawing.Size(60, 60)
+        Me.btnLeft.TabIndex = 6
+        Me.btnLeft.Text = "LEFT"
+        Me.btnLeft.UseVisualStyleBackColor = False
+        '
+        'btnDown
+        '
+        Me.btnDown.BackColor = System.Drawing.Color.Red
+        Me.btnDown.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDown.Location = New System.Drawing.Point(707, 497)
+        Me.btnDown.Name = "btnDown"
+        Me.btnDown.Size = New System.Drawing.Size(60, 60)
+        Me.btnDown.TabIndex = 7
+        Me.btnDown.Text = "DOWN"
+        Me.btnDown.UseVisualStyleBackColor = False
+        '
+        'btnRight
+        '
+        Me.btnRight.BackColor = System.Drawing.Color.Red
+        Me.btnRight.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRight.Location = New System.Drawing.Point(768, 436)
+        Me.btnRight.Name = "btnRight"
+        Me.btnRight.Size = New System.Drawing.Size(60, 60)
+        Me.btnRight.TabIndex = 8
+        Me.btnRight.Text = "RIGHT"
+        Me.btnRight.UseVisualStyleBackColor = False
+        '
+        'lblDirection
+        '
+        Me.lblDirection.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblDirection.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDirection.Location = New System.Drawing.Point(6, 113)
+        Me.lblDirection.Name = "lblDirection"
+        Me.lblDirection.Size = New System.Drawing.Size(249, 23)
+        Me.lblDirection.TabIndex = 9
+        Me.lblDirection.Text = "Direction: "
+        Me.lblDirection.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblSegments
+        '
+        Me.lblSegments.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblSegments.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSegments.Location = New System.Drawing.Point(6, 145)
+        Me.lblSegments.Name = "lblSegments"
+        Me.lblSegments.Size = New System.Drawing.Size(249, 23)
+        Me.lblSegments.TabIndex = 10
+        Me.lblSegments.Text = "Segments: "
+        Me.lblSegments.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblTitle
+        '
+        Me.lblTitle.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblTitle.Font = New System.Drawing.Font("Verdana", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(641, 12)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(193, 50)
+        Me.lblTitle.TabIndex = 12
+        Me.lblTitle.Text = "Snake"
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tmrTime
+        '
+        Me.tmrTime.Interval = 1000
+        '
+        'grpInfo
+        '
+        Me.grpInfo.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.grpInfo.Controls.Add(Me.lblScore)
+        Me.grpInfo.Controls.Add(Me.lblHighScore)
+        Me.grpInfo.Controls.Add(Me.lblTime)
+        Me.grpInfo.Controls.Add(Me.lblDirection)
+        Me.grpInfo.Controls.Add(Me.lblSegments)
+        Me.grpInfo.Location = New System.Drawing.Point(840, 490)
+        Me.grpInfo.Name = "grpInfo"
+        Me.grpInfo.Size = New System.Drawing.Size(266, 182)
+        Me.grpInfo.TabIndex = 15
+        Me.grpInfo.TabStop = False
+        '
+        'lblScore
+        '
+        Me.lblScore.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblScore.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(6, 44)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(249, 23)
+        Me.lblScore.TabIndex = 11
+        Me.lblScore.Text = "Score:"
+        Me.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'grpControls
+        '
+        Me.grpControls.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.grpControls.Controls.Add(Me.picPad)
+        Me.grpControls.Location = New System.Drawing.Point(634, 366)
+        Me.grpControls.Name = "grpControls"
+        Me.grpControls.Size = New System.Drawing.Size(200, 195)
+        Me.grpControls.TabIndex = 16
+        Me.grpControls.TabStop = False
+        '
+        'picPad
+        '
+        Me.picPad.BackColor = System.Drawing.Color.Fuchsia
+        Me.picPad.Location = New System.Drawing.Point(73, 70)
+        Me.picPad.Name = "picPad"
+        Me.picPad.Size = New System.Drawing.Size(60, 60)
+        Me.picPad.TabIndex = 17
+        Me.picPad.TabStop = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.Red
+        Me.btnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.ForeColor = System.Drawing.Color.Black
+        Me.btnReset.Location = New System.Drawing.Point(12, 551)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(174, 74)
+        Me.btnReset.TabIndex = 17
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'frmSnake
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1097, 684)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.btnRight)
+        Me.Controls.Add(Me.btnDown)
+        Me.Controls.Add(Me.btnLeft)
+        Me.Controls.Add(Me.btnUp)
+        Me.Controls.Add(Me.lisSpace)
+        Me.Controls.Add(Me.grpInfo)
+        Me.Controls.Add(Me.grpControls)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmSnake"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Snake"
+        Me.grpInfo.ResumeLayout(False)
+        Me.grpControls.ResumeLayout(False)
+        CType(Me.picPad, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents tmrMain As Timer
+    Friend WithEvents lisSpace As ListBox
+    Friend WithEvents lblHighScore As Label
+    Friend WithEvents lblTime As Label
+    Friend WithEvents btnUp As Button
+    Friend WithEvents btnLeft As Button
+    Friend WithEvents btnDown As Button
+    Friend WithEvents btnRight As Button
+    Friend WithEvents lblDirection As Label
+    Friend WithEvents lblSegments As Label
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents tmrTime As Timer
+    Friend WithEvents grpInfo As GroupBox
+    Friend WithEvents grpControls As GroupBox
+    Friend WithEvents picPad As PictureBox
+    Friend WithEvents lblScore As Label
+    Friend WithEvents btnReset As Button
+End Class
---
+++ Snake/Snake/frmSnake.vb
@@ -0,0 +1,318 @@
+﻿Public Class frmSnake
+    Dim rand As New Random
+    Dim gameWindowSize As Point = New Point(600, 600)
+
+    Dim tileSize As Integer = 25
+
+    Dim tileEmpty As Integer = 0
+    Dim tileWall As Integer = 1
+    Dim tileFood As Integer = 2
+    Dim tileSnake As Integer = 3
+
+    Dim tileColors() As Color = {Color.LawnGreen, Color.Black, Color.Red, Color.Blue}
+
+    Dim spaceAllocation(,) As Integer
+    Dim picAllocation(,) As PictureBox
+    Dim snakeSegments() As Point
+
+    Dim directionalButtons(3) As Button
+    Dim direction As Integer
+    Dim score As Integer
+    Dim highScore As Integer
+
+    'Load Form
+    Private Sub frmSnake_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+
+        'Hide
+        For index = 0 To Me.Controls.Count - 1
+            Me.Controls.Item(index).Visible = False
+        Next
+        btnStart.Visible = True
+        btnReset.Visible = True
+
+        direction = rand.Next(0, 4)
+
+        directionalButtons(0) = btnUp
+        directionalButtons(1) = btnRight
+        directionalButtons(2) = btnDown
+        directionalButtons(3) = btnLeft
+
+        LoadData()
+    End Sub
+
+    'Start Button
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        'Reveal
+        For index = 0 To Me.Controls.Count - 1
+            Me.Controls.Item(index).Visible = True
+        Next
+        StartGame()
+        Me.Controls.Remove(btnStart)
+        Me.Controls.Remove(btnReset)
+    End Sub
+
+    'Reset Button
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        My.Settings.highScore = 0
+        highScore = 0
+        Me.Controls.Remove(btnReset)
+    End Sub
+
+    'Start Game
+    Sub StartGame()
+        tmrMain.Enabled = True
+        tmrTime.Enabled = True
+        ReDim spaceAllocation(gameWindowSize.X / tileSize, gameWindowSize.Y / tileSize) '24,24
+        ReDim picAllocation(gameWindowSize.X / tileSize, gameWindowSize.Y / tileSize)
+        Me.WindowState = FormWindowState.Maximized
+        CreateLayout()
+        CreateSnake()
+        CreateFood()
+        ShowAllocation()
+    End Sub
+
+    'Create Layout at Start of Game
+    Sub CreateLayout()
+        For indexY = 0 To gameWindowSize.Y Step tileSize
+            For indexX = 0 To gameWindowSize.X Step tileSize
+
+                Dim tile As New PictureBox
+                With tile
+                    .Location = New Point(indexX, indexY)
+                    .Size = New Point(tileSize - 1, tileSize - 1)
+                    .BackColor = tileColors(0)
+                End With
+
+                spaceAllocation(indexX / tileSize, indexY / tileSize) = 0
+                picAllocation(indexX / tileSize, indexY / tileSize) = (tile)
+                Me.Controls.Add(tile)
+            Next
+        Next
+        CreateWalls()
+    End Sub
+
+    'Create Walls
+    Sub CreateWalls()
+        For indexY = 0 To 24
+            For indexX = 0 To 24
+                If indexX <= 0 Or indexX >= 24 Or indexY <= 0 Or indexY >= 24 Then
+                    ChangeTile(indexX, indexY, 1)
+                End If
+            Next
+        Next
+    End Sub
+
+    'Finds empty tile space for use returns allocation XY coords
+    Function FindSpace() As Point
+        Dim space(0) As Point
+
+        For indexY = 0 To tileSize - 1
+            For indexX = 0 To tileSize - 1
+                If spaceAllocation(indexX, indexY) = 0 Then
+                    space(space.Length - 1) = New Point(indexX, indexY)
+                    ReDim Preserve space(space.Length)
+                End If
+            Next
+        Next
+
+        Return space(rand.Next(0, space.Length - 1))
+    End Function
+
+    'Create Food
+    Sub CreateFood()
+        Dim space As Point = FindSpace()
+        ChangeTile(space.X, space.Y, 2)
+    End Sub
+
+    'Create Snake
+    Sub CreateSnake()
+        ChangeTile(12, 12, 3)
+        ReDim snakeSegments(0)
+        snakeSegments(0) = New Point(12, 12)
+    End Sub
+
+    'Update Snake
+    Sub UpdateSnake()
+        Static newLoc As Point
+        Static lastLoc As Point
+        Static storeLoc As Point
+        Dim toAdd As Boolean
+
+        Select Case direction
+            Case 0
+                newLoc = New Point(snakeSegments(0).X, snakeSegments(0).Y - 1)
+            Case 1
+                newLoc = New Point(snakeSegments(0).X + 1, snakeSegments(0).Y)
+            Case 2
+                newLoc = New Point(snakeSegments(0).X, snakeSegments(0).Y + 1)
+            Case 3
+                newLoc = New Point(snakeSegments(0).X - 1, snakeSegments(0).Y)
+        End Select
+
+        If spaceAllocation(newLoc.X, newLoc.Y) = 1 Or spaceAllocation(newLoc.X, newLoc.Y) = 3 Then
+            EndGame(spaceAllocation(newLoc.X, newLoc.Y))
+            Exit Sub
+        ElseIf spaceAllocation(newLoc.X, newLoc.Y) = 2 Then
+            toAdd = True
+        End If
+
+        'General Relocation Logic
+        For index = 0 To snakeSegments.Length - 1
+
+            If index = 0 Then
+                'Head
+
+                lastLoc = snakeSegments(0)
+
+                ChangeTile(newLoc.X, newLoc.Y, 3)
+                snakeSegments(0) = newLoc
+
+                ChangeTile(lastLoc.X, lastLoc.Y, 0)
+            Else
+                'Segments
+
+                storeLoc = snakeSegments(index)
+
+                ChangeTile(lastLoc.X, lastLoc.Y, 3)
+                snakeSegments(index) = lastLoc
+
+                lastLoc = storeLoc
+                ChangeTile(storeLoc.X, storeLoc.Y, 0)
+            End If
+        Next
+
+        If toAdd Then
+            AddSegment(lastLoc)
+        End If
+    End Sub
+
+    'Add segment to Snake
+    Sub AddSegment(loc As Point)
+        score += 10
+        ReDim Preserve snakeSegments(snakeSegments.Length)
+        ChangeTile(loc.X, loc.Y, 3)
+        snakeSegments(snakeSegments.Length - 1) = New Point(loc.X, loc.Y)
+        CreateFood()
+    End Sub
+
+    'Change Tile using XY coords
+    Sub ChangeTile(x As Integer, y As Integer, newTile As Integer)
+        spaceAllocation(x, y) = newTile
+        picAllocation(x, y).BackColor = tileColors(newTile)
+        ShowAllocation()
+    End Sub
+
+    'Update Timer
+    Private Sub tmrMain_Tick(sender As Object, e As EventArgs) Handles tmrMain.Tick
+        highScore = If(score > highScore, score, highScore)
+        lblHighScore.Text = "High Score: " & highScore
+        lblScore.Text = "Score: " & score
+        lblSegments.Text = "Segments: " & snakeSegments.Length
+
+        Select Case direction
+            Case 0
+                lblDirection.Text = "Direction: (0) North"
+            Case 1
+                lblDirection.Text = "Direction: (1) East"
+            Case 2
+                lblDirection.Text = "Direction: (2) South"
+            Case 3
+                lblDirection.Text = "Direction: (3) West"
+        End Select
+
+        UpdateSnake()
+    End Sub
+
+    'Time Timer
+    Private Sub tmrTime_Tick(sender As Object, e As EventArgs) Handles tmrTime.Tick
+        Static time As Integer
+        lblTime.Text = "Time: " & time
+        time += 1
+    End Sub
+
+    'End Game
+    Sub EndGame(deathCause As Integer)
+        tmrMain.Enabled = False
+        MessageBox.Show(If(deathCause = 1, "You Slithered into a Wall!", "You Slithered into Yourself for petes snake!"), "Snake", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        SaveData()
+        Application.Restart()
+    End Sub
+
+    'Show Grid data and other information
+    Sub ShowAllocation()
+        lisSpace.Items.Clear()
+        Dim allocationLine As String
+
+        For indexY = 0 To tileSize - 1
+            For indexX = 0 To tileSize - 1
+                allocationLine += spaceAllocation(indexX, indexY).ToString & " "
+            Next
+
+            lisSpace.Items.Add(allocationLine)
+            allocationLine = Nothing
+        Next
+    End Sub
+
+    'DIRECTIONAL BUTTONS
+
+    'Up
+    Private Sub btnUp_Click(sender As Object, e As EventArgs) Handles btnUp.Click
+        direction = 0
+        UpdateButtons()
+        With btnDown
+            .Enabled = False
+            .BackColor = Color.SlateGray
+        End With
+    End Sub
+
+    'Right
+    Private Sub btnRight_Click(sender As Object, e As EventArgs) Handles btnRight.Click
+        direction = 1
+        UpdateButtons()
+        With btnLeft
+            .Enabled = False
+            .BackColor = Color.SlateGray
+        End With
+    End Sub
+
+    'Down
+    Private Sub btnDown_Click(sender As Object, e As EventArgs) Handles btnDown.Click
+        direction = 2
+        UpdateButtons()
+        With btnUp
+            .Enabled = False
+            .BackColor = Color.SlateGray
+        End With
+    End Sub
+
+    'Left
+    Private Sub btnLeft_Click(sender As Object, e As EventArgs) Handles btnLeft.Click
+        direction = 3
+        UpdateButtons()
+        With btnRight
+            .Enabled = False
+            .BackColor = Color.SlateGray
+        End With
+    End Sub
+
+    'Reset Disabled Buttons
+    Sub UpdateButtons()
+        For index = 0 To directionalButtons.Length - 1
+            With directionalButtons(index)
+                .Enabled = True
+                .BackColor = Color.Red
+            End With
+        Next
+    End Sub
+
+    'Save Settings
+    Sub SaveData()
+        My.Settings.highScore = highScore
+    End Sub
+
+    'Load Settings
+    Sub LoadData()
+        highScore = My.Settings.highScore
+    End Sub
+End Class
